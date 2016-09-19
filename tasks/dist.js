d6ebc6b82a4c23f118062dffb4d50a8472f5e510
steubeng <steuben@ge.com>
2016-09-19 11:14:07 -0400

    karma references fail
    with "test" in place, the tutorial now relied on Karma which seems to cause problems.  Rather than build a tutorial centered on testing with Karma, I propose that we remove it.

--- tasks/dist.js
+++ tasks/dist.js
@@ -2,5 +2,5 @@
  * grunt dist builds a minified/vulcanized version of the app in the dist/ folder in preparation for cf push-ing
  */
 module.exports = function(grunt) {
-    grunt.registerTask('dist', ['clean:build', 'jshint', 'test', 'vulcanize', 'copy:dist', 'requirejs']);
+    grunt.registerTask('dist', ['clean:build', 'jshint', 'vulcanize', 'copy:dist', 'requirejs']);
 };
